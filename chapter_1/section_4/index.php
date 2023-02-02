58cbf819e2e74a0c440865045324584d30a573b6
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-02-02 15:58:58 +0700

    calculator operation

---
+++ chapter_1/section_4/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toán tử tính toán trong PHP</title>
+</head>
+<body>
+    <h1>Toán tử tính toán trong PHP</h1>
+    <?php 
+        echo 1 + 1 . '<br>';
+        echo 1 - 1 . '<br>';
+        echo 10 * 2 . '<br>';
+        echo 10 / 2 . '<br>';
+        echo 10 % 2 . '<br>';
+    ?>
+</body>
+</html>
