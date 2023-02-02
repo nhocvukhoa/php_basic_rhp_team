9b1096525399eafbe432bb08cd419433806cd867
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-02-02 22:00:49 +0700

    logic operator

---
+++ chapter_1/section_8/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toán tử logic</title>
+</head>
+<body>
+    <h1>Toán tử logic</h1>
+    <?php 
+        // &&, ||
+        $a = 10;
+        $b = 8;
+
+        if ($a > $b && $a < 4) {
+            echo 'true';
+        } else {
+            echo 'false';
+        }
+    ?>
+</body>
+</html>
