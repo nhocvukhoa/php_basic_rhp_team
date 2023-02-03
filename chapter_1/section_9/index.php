b7eec104aacd82263956aa0a3fb4580509bbc69f
nhocvukhoa <nvanhkhoa148@gmail.com>
2023-02-03 09:06:11 +0700

    switch statement

---
+++ chapter_1/section_9/index.php
@@ -0,0 +1,14 @@
+<?php 
+    $userOption = 1;
+
+    switch ($userOption) {
+        case 1:
+            echo 'Người dùng chọn continue';
+            break;
+        case 2:
+            echo 'Người dùng chọn new game';
+            break;
+        case 3: 
+            echo 'Người dùng thoát';
+    }
+?>
